ab862eae4d560c49bef37c51c0b5992fc03bb2c0
anne55x9 <anne55x9@edu.easj.dk>
2016-10-19 17:15:14 +0200

    11.1 and 11.2

--- Sandbox/BankAccount.cs
+++ Sandbox/BankAccount.cs
@@ -5,7 +5,43 @@ using System.Text;
 
 namespace Sandbox
 {
+
+
     public class BankAccount
     {
+        private string name;
+        private double balance;
+
+        private double deposit;
+        private double withdrawel;
+
+        public BankAccount()
+        {
+            name = "Anne Sofie";
+            balance = 543.7;
+        }
+
+        public void AddDeposit(double deposit)
+        {
+           balance  = deposit + balance;
+        }
+
+        public void AddWithdrawel(double withdrawel)
+        {
+            balance = balance - withdrawel;
+        }
+
+        public string Getname()
+        {
+            return name;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+       
+ 
     }
 }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,7 +8,19 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
+            BankAccount BankAccount1 = new BankAccount();
 
+
+
+            Console.WriteLine($"Account holders name is: {BankAccount1.Getname()}. The balance on the account is: {BankAccount1.GetBalance()} kr");
+
+            BankAccount1.AddDeposit(25);
+
+            Console.WriteLine($"Account holder name is: {BankAccount1.Getname()}. The balance on the account is: {BankAccount1.GetBalance()} kr");
+
+            BankAccount1.AddWithdrawel(200);
+
+            Console.WriteLine($"Account holder name is: {BankAccount1.Getname()}. The balance on the account is: {BankAccount1.GetBalance()} kr");
             // The LAST line of code should be ABOVE this line
         }
     }
